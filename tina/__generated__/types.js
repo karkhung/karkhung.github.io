830a304ec2d36e3b85cea0ce4c13ac4152931911
karkhung <karkhung21@gmail.com>
2026-01-02 11:38:58 +0530

    added

---
+++ tina/__generated__/types.js
@@ -0,0 +1,193 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const PostPartsFragmentDoc = gql`
+    fragment PostParts on Post {
+  __typename
+  title
+  layout
+  episode
+  file
+  time
+  cover
+  description
+  body
+}
+    `;
+export const AudiobookPartsFragmentDoc = gql`
+    fragment AudiobookParts on Audiobook {
+  __typename
+  layout
+  series_slug
+  title
+  series
+  chapter
+  audio_file
+  cover
+  time
+  body
+}
+    `;
+export const PostDocument = gql`
+    query post($relativePath: String!) {
+  post(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...PostParts
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export const PostConnectionDocument = gql`
+    query postConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PostFilter) {
+  postConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...PostParts
+      }
+    }
+  }
+}
+    ${PostPartsFragmentDoc}`;
+export const AudiobookDocument = gql`
+    query audiobook($relativePath: String!) {
+  audiobook(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        hasReferences
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...AudiobookParts
+  }
+}
+    ${AudiobookPartsFragmentDoc}`;
+export const AudiobookConnectionDocument = gql`
+    query audiobookConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: AudiobookFilter) {
+  audiobookConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            hasReferences
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...AudiobookParts
+      }
+    }
+  }
+}
+    ${AudiobookPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    post(variables, options) {
+      return requester(PostDocument, variables, options);
+    },
+    postConnection(variables, options) {
+      return requester(PostConnectionDocument, variables, options);
+    },
+    audiobook(variables, options) {
+      return requester(AudiobookDocument, variables, options);
+    },
+    audiobookConnection(variables, options) {
+      return requester(AudiobookConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, options) => {
+    let url = client.apiUrl;
+    if (options?.branch) {
+      const index = client.apiUrl.lastIndexOf("/");
+      url = client.apiUrl.substring(0, index + 1) + options.branch;
+    }
+    const data = await client.request({
+      query: doc,
+      variables: vars,
+      url
+    }, options);
+    return { data: data?.data, errors: data?.errors, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(
+    createClient({
+      url: "http://localhost:4001/graphql",
+      queries
+    })
+  )
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
