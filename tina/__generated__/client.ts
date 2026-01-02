b26efb7514c26d3ccee8e335b8912999e14163c6
karkhung <karkhung21@gmail.com>
2026-01-02 17:23:24 +0530

    Rebuild admin

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'e9564fb95515f055a3c407c6d92aba5c4379f38e', queries,  });
+export const client = createClient({ cacheDir: 'E:/Github Pages/karkhung.github.io/tina/__generated__/.cache/1767354756246', url: 'https://content.tinajs.io/1.6/content/958e95b9-62bd-4811-8b53-8f4d617c2341/github/main', token: 'e9564fb95515f055a3c407c6d92aba5c4379f38e', queries,  });
 export default client;
   
